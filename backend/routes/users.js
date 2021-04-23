37067883d1866a352dee69640cbcc342e608993e
MatriMariem <meriemmatri1994@gmail.com>
2021-04-23 10:39:19 +0100

    hash passwords, Add more endpoints to usersRouter

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,6 +3,7 @@ const usersRouter = express.Router();
 const bodyParser = require('body-parser');
 const User = require('../models/userModel');
 const jsonParser = bodyParser.json();
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const auth = require('./auth');
 
@@ -39,8 +40,18 @@ usersRouter.get('/:id', async (req, res) => {
 
 usersRouter.post('/signup', async (req, res) => {
   try {
-    const user = new User({...req.body});
-    console.log("my user", user);
+    // SIGNUP INPUT DATA VALIDATION
+    const alreadyExist = await User.findOne({"email": req.body.email});
+    if (alreadyExist) return res.send('Email already exists');
+    // hash password 
+    const salt = await bcrypt.genSalt(10);
+    const hashpassword = await bcrypt.hash(req.body.password, salt);
+
+    let userData = { ...req.body };
+    userData.password = hashpassword;
+
+    // create a new user
+    const user = new User(userData);
     const savedUser = await user.save( (err) => console.log("ERROR", err));
     res.send(savedUser);
   } catch (error) {
@@ -49,9 +60,14 @@ usersRouter.post('/signup', async (req, res) => {
 });
 usersRouter.post('/login', async (req, res) => {
   try {
+    // LOGIN INPUT DATA VALIDATION
+    // Check Email
     const user = await User.findOne({"email": req.body.email});
     if (!user) return res.send('Email not found');
-    if (user.password != req.body.password) return res.send("wrong password");
+    // Check Password
+    const correctPassword = await bcrypt.compare(req.body.password, user.password);
+    if (!correctPassword) return res.send("wrong password");
+    // Create a token and send it to the client
     const token = await jwtr.sign({"_id": user._id}, process.env.SECRET_TOKEN);
     console.log(token)
     res.header('auth-token', token).send(token);
@@ -60,20 +76,66 @@ usersRouter.post('/login', async (req, res) => {
   res.json({ message: error });
 }});
 
-usersRouter.patch('/:id', async (req, res) => {
+usersRouter.patch('/:id', auth, async (req, res) => {
   try {
+    const user = await User.findById(req.params.id);
+    if (!user) return res.send("User already doesn't exist");
+
+    if (req.user._id != user._id) return res.send("Access Denied");
+    let userData = {...req.body};
+    if (userData.id) delete userData.id;
+    if (userData._id) delete userData._id;
+    if (userData.password) delete userData.password;
+
     const updated = await User.updateOne(
       { _id: req.params.id },
-      { $set: {...req.body}}
+      { $set: userData}
     );
-    // res.send("updated");
+    res.send("user updated!");
   } catch (error) {
     res.json({ message: error });
   }
 });
 
-usersRouter.delete('/:id', async (req, res) => {
+usersRouter.patch('/:id/reset_password', auth, async (req, res) => {
   try {
+    const user = await User.findById(req.params.id);
+    if (!user) return res.send("User already doesn't exist");
+
+    if (req.user._id != user._id) return res.send("Access Denied");
+
+    if (!req.body.password) return res.send("Please enter the old password!");
+    const correctPassword = await bcrypt.compare(req.body.password, user.password);
+    if (!correctPassword) return res.send("wrong password");
+
+    if (!req.body.new_password) return res.send("Please enter the new password!");
+
+    const salt = await bcrypt.genSalt(10);
+    const hashpassword = await bcrypt.hash(req.body.new_password, salt);
+
+    const updated = await User.updateOne(
+      { _id: req.params.id },
+      { $set: {"password": hashpassword}}
+    );
+    res.send("user updated!");
+  } catch (error) {
+    res.json({ message: error });
+  }
+});
+
+usersRouter.delete('/:id', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user) return res.send("User already doesn't exist");
+
+    if (req.user._id != user._id) return res.send("Access Denied");
+
+    if (!req.body.password) return res.send("Please enter your password");
+
+    const correctPassword = await bcrypt.compare(req.body.password, user.password);
+    if (!correctPassword) return res.send("wrong password");
+
     const removedUser = await User.remove({ _id: req.params.id });
     res.json(removedUser);
   } catch (error) {
