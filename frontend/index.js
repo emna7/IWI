796c20d31e053e8008a5d8f7069bd79e863319ab
Edin93 <il_matador@hotmail.fr>
2021-05-02 11:49:44 +0100

    added react redux setup and folders structure

--- frontend/index.js
+++ frontend/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const title = 'IWI';
+import App from './App';
+import store from './redux/store.js';
+import { Provider, } from 'react-redux';
 
 ReactDOM.render(
-  <div>{title}</div>,
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
   document.getElementById('app')
 );
